fc241a48e34bac370fab6a138c5281261ea9725d
caiwen <caiwen@globalegrow.com>
2018-11-25 17:14:57 +0800

    修复读取路径错误的问题

--- src/util/config.py
+++ src/util/config.py
@@ -2,17 +2,18 @@
 工具类，获取配置信息
 """
 import configparser
-
+import approot
 
 class Config:
 
     def __init__(self):
         # 配置文件目录
-        self.file = 'config/config.ini'
+        self.file = '/config/config.ini'
 
     # 获取配置
     def get_config(self, section, name):
         conf = configparser.ConfigParser()
-        conf.read(self.file)
+        config_path = approot.get_root() + self.file
+        conf.read(config_path)
         name = conf.get(section, name)
         return name
